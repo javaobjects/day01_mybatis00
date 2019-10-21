d49e6a789eaf7f2dad94ba0bce7ec614368aae93
yanbo0039 <yanbo0039@yeah.net>
2019-10-21 13:40:33 +0800

    序列

--- src/com/tencent/test/Test.java
+++ src/com/tencent/test/Test.java
@@ -13,8 +13,9 @@ public class Test {
 //		Emp emp = empDao.selectEmpByEmpno(7788);
 //		System.out.println(emp);
 		//新增
-//		Emp emp = new Emp("1111","张三","程序员");
-//		System.out.println("新增" + empDao.insertEmp(emp));
+		Emp emp = new Emp("0","张三","程序员");
+		System.out.println("新增" + empDao.insertEmp(emp));
+		System.out.println("参数emp: " + emp);
 		//更新
 		
 //		Emp emp = new Emp();
@@ -23,7 +24,7 @@ public class Test {
 //		System.out.println("更新" + empDao.updateEmp(emp));
 		
 		//删除
-		System.out.println("删除" + empDao.deleteEmp(1111));
+//		System.out.println("删除" + empDao.deleteEmp(1111));
 	}
 
 }
